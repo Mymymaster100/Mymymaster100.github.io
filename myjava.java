014564fc2a2ae77e0955f229c16c6e50df8b9929
Miles <msanders.github@gmail.com>
2024-07-29 14:14:10 -0500

    15

--- myjava.java
+++ myjava.java
@@ -2,11 +2,9 @@ var click=0;
 var c = document.getElementById("click");
 function hi(){
     click+=1;
-    alert("click = " + click);
     c.innerHTML = click;
 }
 function reset(){
     click=0;
-    alert("click = " + click);
     c.innerHTML = click;
 }
