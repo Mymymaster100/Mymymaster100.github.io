d968f2ca1338512e88cee81dd2ef3d5fc6c949fd
Miles <msanders.github@gmail.com>
2024-07-31 13:32:43 -0500

    40

--- myjava.java
+++ myjava.java
@@ -1,8 +1,10 @@
 var clicks = 0;
 var c = document.getElementById("clicking");
 var c2 = document.getElementById("clicking2");
+const button = document.querySelector("button");
 function click() {
     clicks += 1;
-    c.innerHTML =  "You Have " + clicks + " Clicks";
-    c2.innerHTML = clicks + " - Miles Clicker";
+    c.textContent =  "You Have " + clicks + " Clicks";
+    c2.textContent = clicks + " - Miles Clicker";
 }
+button.addEventListener("click", click);
