ae4d1ebab84b6c5a107deaeca5e0dc8d76a7399d
mymymaster100 <msanders.github@gmail.com>
2024-08-03 17:35:51 -0500

    51

---
+++ script2.js
@@ -0,0 +1,16 @@
+var h = document.getElementById("hash");
+String.prototype.hashCode = function() {
+    var hash = 0,
+      i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0;
+    }
+    return hash;
+  }
+function MakeTheHash() {
+    var hash = document.getElementById("hashing").value;
+    h.textContent = hash.hashCode();
+}
