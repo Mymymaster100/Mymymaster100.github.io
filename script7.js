bcebac70041314fed028807062d39568f8566765
Mymymaster100 <msanders.github@gmail.com>
2024-10-04 10:52:30 -0500

    Create script7.js

---
+++ script7.js
@@ -0,0 +1,22 @@
+var star = 0;
+
+function oneStar() {
+  star = 1;
+}
+
+function twoStar() {
+  star = 2;
+}
+
+function threeStar() {
+  star = 3;
+}
+
+function fourStar() {
+  star = 4;
+}
+
+function fiveStar() {
+  star = 5;
+}
+
